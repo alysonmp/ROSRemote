85add4989b9e4fb2753897bae56992a5088bca5b
alysonmp <alysonmp@gmail.com>
2017-01-23 11:48:53 -0200

    Add files via upload

---
+++ src/roscommands.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('cloud_ros')
+import rospy
+from cloud_ros.srv import *
+from pySpacebrew.spacebrew import Spacebrew
+from std_msgs.msg import String
+import rosgraph.masterapi
+import time
+import os
+import subprocess
+import threading
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+def rosCommandsFunctions(comando, brew):
+
+	#implementar rospack list "nome"
+
+	comandoSplit = comando.split(" ")
+	if len(comandoSplit) == 1:
+		data = {'comandoRos':'roscommands', 'funcao':'roscommands', 'acao':'enviar', 'commands':comandoSplit[0]}
+		brew.publish("Publisher", data)
+		rospy.logwarn("Comando enviado = "+comando)
+	elif (comandoSplit[1] == "set_robot"):
+		data = {'comandoRos':'roscommands', 'funcao':'set_robot', 'acao':'enviar', 'commands':comandoSplit[2]}
+		brew.publish("Publisher", data)
+		rospy.logwarn("Comando enviado = "+comando)
+	else:
+		rospy.logwarn("Sintaxe do comando incorreta")	
+	
+'''INICIO ROSCOMMANDS'''
+def set_robot(brew, commands):
+	global robot 
+	robot = commands
+
+def roscommands(brew, commands):
+	global robot
+
+	vel = Twist()
+	global proc
+	if(commands == "up"): 
+		vel.linear.x = 2
+		vel.linear.y = 0
+		vel.linear.z = 0
+
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = 0
+	elif(commands == "down"):
+		vel.linear.x = -2
+		vel.linear.y = 0
+		vel.linear.z = 0
+
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = 0
+	elif(commands == "right"):
+		vel.linear.x = 0
+		vel.linear.y = 0
+		vel.linear.z = 0
+
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = -2
+	elif(commands == "left"):
+		vel.linear.x = 0
+		vel.linear.y = 0
+		vel.linear.z = 0
+
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = 2
+
+	robots = robot.split(":")
+
+	for rob in robots:
+		pub = rospy.Publisher(rob+'/cmd_vel', Twist, queue_size=10)
+		pub.publish(vel)
+
+		vel.linear.x = 0
+		vel.linear.y = 0
+		vel.linear.z = 0
+
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = 0
+	
+		time.sleep(0.5)
+
+		pub = rospy.Publisher(rob+'/cmd_vel', Twist, queue_size=10)
+		pub.publish(vel)
+
+'''FIM ROSCOMMANDS'''
+
+
--- src/rostopic.py
+++ src/rostopic.py
@@ -5,10 +5,20 @@ import rospy
 from cloud_ros.srv import *
 from pySpacebrew.spacebrew import Spacebrew
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 import rosgraph.masterapi
 import time
 import os
 import subprocess
+
+import importlib
+
+#from rosgraph_msgs.msg import Log
+from geometry_msgs.msg import Twist
+
+#import std_msgs.msg
+#import geometry_msgs.msg
+#import rosgraph_msgs.msg
 
 def rostopicFunctions(comando, brew):
 	global stop_
@@ -54,6 +64,24 @@ def rostopicList(brew, topic):
 
 
 '''INICIO ROSTOPIC ECHO'''
+def ros2xml(msg, name, depth=0):
+	xml = "";
+	tabs = "\t"*depth
+
+	if hasattr(msg, "_type"):
+	    type = msg._type
+	    xml = xml + tabs + "<" + name + " type=\"" + type + "\">\n"
+
+	    try:
+		for slot in msg.__slots__:
+		    xml = xml + ros2xml(getattr(msg, slot), slot, depth=depth+1)
+	    except:
+		xml = xml + tabs + str(msg)
+	    xml = xml + tabs + "</" + name + ">\n"
+	else:
+	    xml = xml + tabs + "<" + name + ">" + str(msg) + "</" + name + ">\n"
+	return xml
+
 def callback(resp):
 
 	global brew_
@@ -61,21 +89,34 @@ def callback(resp):
 
 	stop_ = False
 
-	dados = resp.data
-	rospy.logwarn(stop_)
+	#dados = resp.data
+	rospy.logwarn("fora do while")
 	while(stop_ == False):
-		data= {'dados':dados, 'title':"Resultados de rostopic echo ", 'acao':'receber'}
+		rospy.logwarn("dentro do while")
+		xml = ros2xml(resp, "")
+		data= {'dados':xml, 'title':"Resultados de rostopic echo ", 'acao':'receber'}
 		brew_.publish("Publisher", data)
 		time.sleep(1)
 		rospy.logwarn("aqui = "+str(stop_))
 
 	rospy.logwarn(stop_)
 
+
 def rostopicEcho(brew, topic):
 	global brew_
 	brew_ = brew
 
-	rospy.Subscriber(topic, String, callback)
+	proc = subprocess.Popen(["rostopic type "+topic], stdout=subprocess.PIPE, shell=True)
+	(dados, err) = proc.communicate()
+
+	dado = dados.split("/")
+	dado[0] += ".msg"
+
+	mod = __import__(dado[0], fromlist=[dado[1]])
+
+	klass = getattr(mod, dado[1].strip())
+
+	rospy.Subscriber(topic, klass, callback)
 '''FIM ROSTOPIC ECHO'''
 
 
